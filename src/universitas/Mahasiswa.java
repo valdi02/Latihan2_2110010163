a93cff11832a2eda30fbfc1f2623fdfda08822c2
valdi02 <mvaldierlangga1@gmail.com>
2023-06-13 08:14:35 +0700

    revisi

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -9,8 +9,7 @@ package universitas;
  *
  * @author valdi
  */
-public class Mahasiswa {
-    constructor(nama, prodi) {
+public class Mahasiswa { 
     private String nama;
     private String prodi;
     private String nim;
@@ -18,5 +17,28 @@ public class Mahasiswa {
     public Mahasiswa(String nama, String prodi) {
         this.nama = nama;
         this.prodi = prodi;
+        this.nim = nim;
+}
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+     public String getNim() {
+        return nim;
+    }
+    public void setNim(String nim) {
+        this.nim = nim;
+    
 }
 }
--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -10,5 +10,17 @@ package universitas;
  * @author valdi
  */
 public class MahasiswaBeraksi {
-    
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("Muhammad Valdi Erlangga", "Informatika", "2110010163");
+        System.out.println("Nama: " + mhs.getNama());
+        System.out.println("Prodi: " + mhs.getProdi());
+        System.out.println("nim: " + mhs.getnim());
+        
+
+        mhs.setNama("M Valdi Erlangga");
+        mhs.setProdi("Sistem Informasi");
+        System.out.println("Nama (setelah diubah): " + mhs.getNama());
+        System.out.println("Prodi (setelah diubah): " + mhs.getProdi());
+    }
 }
+
